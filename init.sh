09d3852e5baf224b54279eeae92f31eb950e4111
JorisRijs <joris@freya22.nl>
2020-11-10 21:39:21 +0100

    updated the dot-files and the init script

--- init.sh
+++ init.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-# get the absolute path of out dotfiles repository
-DIR=$(cd `dirname $0` && pwd)`
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # overwrite of the existing symlinks of necessary
-ln -sf $DIR/.bashrc ~/.bashrc
-ln -sf $DIR/.vimrc ~/.vimrc
-ln -sf $DIR/.tmux.conf ~/.tmux.conf
-ln -sf $DIR/.zshrc ~/.zshrc
+ln -sf ~/Dot-Files/.bashrc ~/.bashrc
+ln -sf ~/Dot-Files/.vimrc ~/.vimrc
+ln -sf ~/Dot-Files/.tmux.conf ~/.tmux.conf
+ln -sf ~/Dot-Files/.zshrc ~/.zshrc
 
 
 # application of the new dot files
-. ~/.bashrc
-. ~/.vimrc
-. ~/.zshrc
-. ~/.tmux.conf
+source ~/.bashrc
+source ~/.vimrc
+source ~/.zshrc
+source ~/.tmux.conf
 
 
