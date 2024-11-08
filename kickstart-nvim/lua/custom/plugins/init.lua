beb4cfe42535445b22ecd9a137035a4261a3bb70
Joris Rijs <joris@freya22.nl>
2024-11-08 01:46:55 +0100

    Started on obsidian.nvim setup

--- kickstart-nvim/lua/custom/plugins/init.lua
+++ kickstart-nvim/lua/custom/plugins/init.lua
@@ -21,7 +21,7 @@ return {
         -- Size can be a number or function which is passed the current terminal
         size = 20,
         -- Key mapping to toggle terminal
-        open_mapping = [[<c-\>]],
+        open_mapping = [[<C-\>]],
         -- Hide the number column in terminal buffers
         hide_numbers = true,
         -- Shade terminal background
@@ -70,5 +70,39 @@ return {
       -- Apply the keymaps when terminal is opened
       vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
     end,
+  },
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/vaults/personal',
+        },
+        {
+          name = 'work',
+          path = '~/vaults/work',
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
   },
 }
